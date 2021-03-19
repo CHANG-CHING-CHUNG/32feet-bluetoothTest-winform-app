654dd71be1778061d9fceb11dad29f2711def359
John <54635661+CHANG-CHING-CHUNG@users.noreply.github.com>
2021-03-19 14:21:02 +0800

    Init a winform bluetoothTest app

---
+++ bluetoothTest2/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace bluetoothTest2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxDeviceList = new System.Windows.Forms.ListBox();
+            this.btnDiscover = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBoxDeviceList
+            // 
+            this.listBoxDeviceList.FormattingEnabled = true;
+            this.listBoxDeviceList.ItemHeight = 12;
+            this.listBoxDeviceList.Location = new System.Drawing.Point(12, 12);
+            this.listBoxDeviceList.Name = "listBoxDeviceList";
+            this.listBoxDeviceList.Size = new System.Drawing.Size(701, 232);
+            this.listBoxDeviceList.TabIndex = 0;
+            // 
+            // btnDiscover
+            // 
+            this.btnDiscover.Location = new System.Drawing.Point(12, 281);
+            this.btnDiscover.Name = "btnDiscover";
+            this.btnDiscover.Size = new System.Drawing.Size(159, 46);
+            this.btnDiscover.TabIndex = 1;
+            this.btnDiscover.Text = "搜尋藍芽裝置";
+            this.btnDiscover.UseVisualStyleBackColor = true;
+            this.btnDiscover.Click += new System.EventHandler(this.btnDiscover_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDiscover);
+            this.Controls.Add(this.listBoxDeviceList);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxDeviceList;
+        private System.Windows.Forms.Button btnDiscover;
+    }
+}
+
---
+++ bluetoothTest2/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bluetoothTest2
+{
+    
+    public partial class Form1 : Form
+    {
+        private delegate void DelShowDeviceLsit(string blueName, string blueAddress, string isConneted);
+        BluetoothServicecs BluetoothServicecs;
+
+        bool isDiscovering = false;
+        public Form1()
+        {
+            InitializeComponent();
+            this.BluetoothServicecs = new BluetoothServicecs();
+        }
+
+        private void btnDiscover_Click(object sender, EventArgs e)
+        {
+            listBoxDeviceList.Items.Clear();
+            if (isDiscovering)
+            {
+                MessageBox.Show("正在搜尋中! 請物重覆點擊!");
+                return;
+            }
+            Thread t = new Thread(StartDiscover);
+            t.Start();
+            isDiscovering = true;
+        }
+
+        private void StartDiscover()
+        {
+            BluetoothServicecs.DiscoverDevices();
+            BluetoothServicecs.lanYaList.ForEach(device =>
+            {
+                DelShowDeviceLsit del = new DelShowDeviceLsit(AddDevicetoListBox);
+                this.Invoke(del,device.blueName, device.blueAddress.ToString(),device.IsConnected.ToString());
+                
+            });
+            isDiscovering = false;
+            BluetoothServicecs.lanYaList.Clear();
+        }
+
+        private void AddDevicetoListBox(string blueName, string blueAddress, string isConnected)
+        {
+            listBoxDeviceList.Items.Add("裝置名稱： " + blueName + "   " + "裝置MAC： " + blueAddress + "   " + "連線中： " + isConnected);
+        }
+    }
+}
